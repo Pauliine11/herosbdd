26d75ad863aa32fd690a856459dd33ecc44a9cca
Pauliine11 <paulinevires@.com>
2025-09-15 16:58:37 +0200

    base de donnée

--- app/Views/404.php
+++ app/Views/404.php
@@ -1,7 +1,8 @@
 <?php 
 require_once(__DIR__ . '/partials/head.php');
 ?>
-<h1 class="text-danger">Erreur</h1>
+<!-- <h1 class="text-danger">Erreur</h1> -->
+<img class="error-img" src="public/img/warnin.png" alt="image erreur 404">
 
 <?php 
 require_once(__DIR__ . '/partials/footer.php');
