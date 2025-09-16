76358ce4f40ff6ece982f850c1720339735260da
Pauliine11 <paulinevires@.com>
2025-09-16 12:12:05 +0200

    récupération du formulaire

--- app/Views/404.php
+++ app/Views/404.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . '/partials/head.php');
 ?>
 <!-- <h1 class="text-danger">Erreur</h1> -->
-<img class="error-img" src="public/img/warnin.png" alt="image erreur 404">
+<img class="error-img" src="public/img/cat404.jpg" alt="image erreur 404">
 
 <?php 
 require_once(__DIR__ . '/partials/footer.php');
--- app/Views/register.view.php
+++ app/Views/register.view.php
@@ -5,30 +5,53 @@ require_once(__DIR__ . '/partials/head.php');
 <?php
     require_once 'partials/head.php';
 ?>
-<h1>Formulaire</h1>
+<h1 class="mt-5">Formulaire d'inscription</h1>
 
 <section id="contact" class="contact">
-    <form action="#" method="post">
+    <form method="POST">
         <div class="container mt-5">
+
             <div class="form-group">
-                <label for="email">Adresse e-mail</label>
-                <input type="email" class="form-control" name="email" placeholder="Email" required>
+                <label for="pseudo" class="form-label" >Pseudo</label>
+                <input type="text" class="form-control" name="pseudo" placeholder="Pseudo">
+                <?php
+                    if(isset($arrayError['pseudo'])){
+                      ?>
+                      <p class="text-danger"><?= $arrayError['pseudo']?></p>
+                      <?php  
+                    }
+                ?>
             </div>
 
             <div class="form-group">
-                <label for="name">Prénom</label>
-                <input type="text" class="form-control" name="name" placeholder="Prénom" required>
+                <label for="email" class="form-label mt-3">Adresse e-mail</label>
+                <input type="email" class="form-control" name="email" placeholder="Email">
+                <?php
+                    if(isset($arrayError['email'])){
+                      ?>
+                      <p class="text-danger"><?= $arrayError['email']?></p>
+                      <?php  
+                    }
+                ?>
             </div>
 
             <div class="form-group">
-                <label for="message">Message</label>
-                <textarea class="form-control" name="message" rows="4" placeholder="Vôtre message"></textarea>
+                <label for="password" class="form-label mt-3">Mot de passe:</label>
+                <input type="password" class="form-control"  id="password" name="password" minlength="8" placeholder="Entrez le mot de passe">
+                <?php
+                    if(isset($arrayError['password'])){
+                      ?>
+                      <p class="text-danger"><?= $arrayError['password']?></p>
+                      <?php  
+                    }
+                ?>
             </div>
 
-            <button type="submit" class="btn btn-primary mt-2">Envoyer</button>
+            <button type="submit" class="btn btn-info mt-3">S'inscrire</button>
         </div>
     </form>
 </section>
+
 <?php 
 require_once(__DIR__ . '/partials/footer.php');
 ?>
