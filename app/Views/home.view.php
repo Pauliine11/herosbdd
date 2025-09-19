ca7b11e450fd662652f6cd39cd1f395a067263da
Pauliine11 <paulinevires@.com>
2025-09-19 12:15:38 +0200

    ajout de heros

--- app/Views/home.view.php
+++ app/Views/home.view.php
@@ -4,6 +4,22 @@ require_once(__DIR__ . '/partials/head.php');
 
 <h1>Bienvenue ! </h1>
 
+<?php
+    if($heros){
+        foreach($heros as $value){
+            ?>
+                <div style="width: 18rem;">
+                    <img class="card-img-top rounded-circle border-4 border border-dark" src="public/img/<?= $value['image'] ?>" alt="Image de <?php echo $value['name'] ?>">
+                    <h2><?= $value['name']?></h2>
+                    <a href="/hero?id=<?= $value['id']?>" class="btn btn-outline-info d-flex justify-content-center">Voir +</a>
+                </div>
+            <?php
+        }
+    }else{
+        echo "<p>Aucun personnage disponible, ils sont tous en mission.</p>";
+    } 
+?>
+
 <?php 
 require_once(__DIR__ . '/partials/footer.php');
 ?>
